e6a00a7f61052412e976e252d768925e2bb0eb64
ider <326737833@qq.com>
2017-09-08 14:11:12 +0800

    change create date

--- _site/assets/js/analytics.js
+++ _site/assets/js/analytics.js
@@ -1,7 +1,7 @@
 // analytics.js
 
 // date range
-var firstDay = new Date(2016, 9, 1);
+var firstDay = new Date(2017, 9, 8);
 function siteSinceDays() {
     var d = new Date();
     var today = new Date(d.getFullYear(), d.getMonth(), d.getDate());
